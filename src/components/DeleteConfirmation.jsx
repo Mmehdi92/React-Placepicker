9661a40a756d9d0d95794cceeed29f96e6b22c7e
Mehdi Zymberi <zymberi.mz@gmail.com>
2024-07-28 21:40:24 +0200

    changed time from 300 sec to 3  sec

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import ProgressBar from "./ProgressBar";
 
-const TIMER = 300000;
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
   useEffect(() => {
